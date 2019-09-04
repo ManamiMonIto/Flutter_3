83874ce7e71f4380ddf021b6e83fa18d385bf733
Manami Ito <manami.j27@gmail.com>
2019-09-05 00:27:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import './answer.dart';
+import './question.dart';
+import './quiz.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  static const questions = [
+    {
+      'questionText': 'What is your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What is your favorite animal?',
+      'answers': ['Elephant', 'Gorilla', 'Penguine', 'Turtle']
+    },
+    {
+      'questionText': 'What is your favorite season?',
+      'answers': ['Spring', 'Summer', 'Autum', 'Winter']
+    },
+  ];
+
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions');
+    } else {
+      print('no more question!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < questions.length
+            ? Quiz()
+            : Center(child: Text('You did it!')),
+      ),
+    );
+  }
+}
